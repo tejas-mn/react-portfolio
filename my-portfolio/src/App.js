97a428c6f592ccb5e6d9d1a268248bb092123325
tejas-mn <68629215+tejas-mn@users.noreply.github.com>
2024-01-29 18:40:39 +0000

    css

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -19,10 +19,30 @@ function App() {
         </div>
         <div className="btm-txt">
         <p>
-         Integer semper dui mauris, in faucibus sem sollicitudin eget adipiscInteger semper dui mauris, in faucibus sem sollicitudin eget.Lorem ipsum dolor sit amet, consectetur adipiscInteger semper dui mauris, in faucibus sem sollicitudin eget.
+         Integer semper dui mauris, in faucibus sem sollicitudin eget adipiscInteger semper dui mauris, in faucibus sem sollicitudin eget.Lorem ipsum dolor sit amet, consectetur adipiscInteger semper dui mauris.
           </p>
       
-  
+          <div className='skills2'>
+
+            <div>
+              <li>Javascript</li>
+            </div>
+
+            <div>
+              <li>C# .NET </li>
+            </div>
+
+            <div>
+              <li>Javascript</li>
+            </div>
+
+            <div>
+              <li>C# .NET </li>
+            </div>
+
+            </div>
+            <hr></hr>
+            <p>Github | Linkedin | Leetcode</p>
         </div>
         
       </div>
@@ -39,9 +59,50 @@ function App() {
           <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur faucibus lacus pellentesque varius. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque gravida quam quis est tincidunt vulputate. Mauris fermentum felis vitae libero luctus, facilisis luctus metus feugiat. Phasellus sed tempor justo, euismod gravida purus. Pellentesque efficitur mattis congue. Etiam ac dui vitae augue rhoncus luctus sed vitae massa. Aliquam laoreet sollicitudin suscipit. Pellentesque enim tellus, faucibus in odio quis, tristique congue nibh. Pellentesque consectetur vel lorem a rhoncus. In volutpat nisl erat, at luctus elit congue ut. Etiam a metus a justo interdum ullamcorper. Ut ornare vitae turpis id cursus. Cras cursus justo sapien, eget fringilla dui ullamcorper sit amet. Integer semper dui mauris, in faucibus sem sollicitudin eget.
           </p>
+          
         <hr></hr>
           <h2>Skills</h2>
-        
+          <div className='skills'>
+
+              <div>
+                <li>Javascript</li>
+              </div>
+
+              <div>
+                <li>C# .NET </li>
+              </div>
+
+              <div>
+                <li>Java</li>
+              </div>
+
+              <div>
+                <li>Javascript</li>
+              </div>
+
+              <div>
+                <li>C# .NET </li>
+              </div>
+
+              <div>
+                <li>Java</li>
+              </div>
+
+              <div>
+                <li>Javascript</li>
+              </div>
+
+              <div>
+                <li>C# .NET </li>
+              </div>
+
+              <div>
+                <li>Java</li>
+              </div>
+
+              
+
+          </div>
          
         </section>
       </div>
