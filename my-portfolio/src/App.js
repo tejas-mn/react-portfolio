d8a573e1f27ff6a3bbcc69df5b71ee16d99c8f03
tejas-mn <68629215+tejas-mn@users.noreply.github.com>
2024-01-29 15:44:40 +0000

    first commit

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -10,7 +10,10 @@ function App() {
         <div className="pic">
 
         </div>
-      
+      <div className="pic-desc">
+        <h2>Tejas M N</h2>
+        <p>SDET | CSGI</p>
+      </div>
         <div className="desc">
 
         </div>
@@ -28,9 +31,10 @@ function App() {
           <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur faucibus lacus pellentesque varius. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque gravida quam quis est tincidunt vulputate. Mauris fermentum felis vitae libero luctus, facilisis luctus metus feugiat. Phasellus sed tempor justo, euismod gravida purus. Pellentesque efficitur mattis congue. Etiam ac dui vitae augue rhoncus luctus sed vitae massa. Aliquam laoreet sollicitudin suscipit. Pellentesque enim tellus, faucibus in odio quis, tristique congue nibh. Pellentesque consectetur vel lorem a rhoncus. In volutpat nisl erat, at luctus elit congue ut. Etiam a metus a justo interdum ullamcorper. Ut ornare vitae turpis id cursus. Cras cursus justo sapien, eget fringilla dui ullamcorper sit amet. Integer semper dui mauris, in faucibus sem sollicitudin eget.
           </p>
-          <div className="box">
-
-          </div>
+        <hr></hr>
+          <h2>Skills</h2>
+          
+         
         </section>
       </div>
 
