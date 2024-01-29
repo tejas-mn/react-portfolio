dd5daebd2e75d059757aad3f1ccabc495750e19a
tejas-mn <68629215+tejas-mn@users.noreply.github.com>
2024-01-29 15:15:35 +0000

    first commit

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,41 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+
+      <div className="left">
+        <div className="pic">
+
+        </div>
+      
+        <div className="desc">
+
+        </div>
+      </div>
+
+      <div className="right">
+        <div className="right-nav">
+          <h2>About |</h2>
+          <h2>Experience |</h2>
+          <h2>Work </h2>
+        </div>
+
+        <section>
+          <h2>About Me</h2>
+          <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec efficitur faucibus lacus pellentesque varius. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque gravida quam quis est tincidunt vulputate. Mauris fermentum felis vitae libero luctus, facilisis luctus metus feugiat. Phasellus sed tempor justo, euismod gravida purus. Pellentesque efficitur mattis congue. Etiam ac dui vitae augue rhoncus luctus sed vitae massa. Aliquam laoreet sollicitudin suscipit. Pellentesque enim tellus, faucibus in odio quis, tristique congue nibh. Pellentesque consectetur vel lorem a rhoncus. In volutpat nisl erat, at luctus elit congue ut. Etiam a metus a justo interdum ullamcorper. Ut ornare vitae turpis id cursus. Cras cursus justo sapien, eget fringilla dui ullamcorper sit amet. Integer semper dui mauris, in faucibus sem sollicitudin eget.
+          </p>
+          <div className="box">
+
+          </div>
+        </section>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
