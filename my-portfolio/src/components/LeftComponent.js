a9408cdb254dcebd23a14d26d3d00557c20ae847
tejas-mn <68629215+tejas-mn@users.noreply.github.com>
2024-01-31 18:26:48 +0000

    refactor

--- my-portfolio/src/components/LeftComponent.js
+++ my-portfolio/src/components/LeftComponent.js
@@ -1,14 +1,16 @@
 export  function LeftComponent(){
     return (
     <div className="left">
+       <div className="pic-container">
         <div className="pic">
-          <img src="https://img.freepik.com/free-vector/gradient-t-logo-template_23-2149372726.jpg?w=740&t=st=1706544579~exp=1706545179~hmac=fbddfea98bf54dc5a4603184bd23eef36513d5fbce5f0797b1b9e8b6c00f780d"></img>
-        </div>
-        <div className="pic-desc">
-          <h2>Tejas M N</h2>
-          <p><b><i>SDET | CSGI</i></b></p>
-        </div>
-        
+            <img src="https://img.freepik.com/free-vector/gradient-t-logo-template_23-2149372726.jpg?w=740&t=st=1706544579~exp=1706545179~hmac=fbddfea98bf54dc5a4603184bd23eef36513d5fbce5f0797b1b9e8b6c00f780d"></img>
+          </div>
+          <div className="pic-desc">
+            <h2>Tejas M N</h2>
+            <p><b><i>SDET | CSGI</i></b></p>
+          </div>
+          
+       </div>
         <div className="desc">
         </div>
         
