00fb931477e47ba467c9b85c550ea044b910c058
tejas-mn <68629215+tejas-mn@users.noreply.github.com>
2024-02-04 08:16:08 +0000

    progress

--- my-portfolio/src/components/Work.js
+++ my-portfolio/src/components/Work.js
@@ -4,19 +4,23 @@ import React, { useState } from 'react';
 import { ProjectsList } from "./ProjectsList";
 
 export function Work(){
-  const [isVisible, setIsVisible] = useState(false);
-  
-    return ( 
+  const [isGridView, setGridView] = useState(false);
+  const [btnText, setbtnText] = useState('Grid view');
+
+  return ( 
     <section>
-        <h2>Projects | Blogs</h2>
-
-        <a href="#" onClick={()=>{setIsVisible((prev)=>!prev)}} style={{color:'white'}}>Toggle View</a>
-        
+      <div style={{display:'flex', justifyContent:'space-between', alignItems:'center'}}>
+        <h2 style={{color:'white'}}>Projects | Blogs</h2>
+        <button 
+          onClick={()=>{
+            setGridView((prev)=>!prev);
+            (!isGridView)?setbtnText('List View'):setbtnText('Grid View');
+          }} 
+          style={{height:'30px', borderRadius:'5px', cursor:'pointer'}}>{btnText}</button>
+      </div>
         {
-            (isVisible)?(<ProjectsGrid/>):(<ProjectsList/>)
+            (isGridView)?(<ProjectsGrid/>):(<ProjectsList/>)
         }
-
-    
     </section>
     )
-  }
+}
