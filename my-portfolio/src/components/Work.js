8c498394f884729c44f0506fa9a61ed4370b8bd4
tejas-mn <68629215+tejas-mn@users.noreply.github.com>
2024-02-04 15:37:05 +0000

    progress

--- my-portfolio/src/components/Work.js
+++ my-portfolio/src/components/Work.js
@@ -11,12 +11,12 @@ export function Work(){
     <section>
       <div style={{display:'flex', justifyContent:'space-between', alignItems:'center'}}>
         <h2 style={{color:'white'}}>Projects | Blogs</h2>
-        <button 
+        <button className="view-btn"
           onClick={()=>{
             setGridView((prev)=>!prev);
             (!isGridView)?setbtnText('List View'):setbtnText('Grid View');
           }} 
-          style={{height:'30px', borderRadius:'5px', cursor:'pointer'}}>{btnText}</button>
+        >{btnText}</button>
       </div>
         {
             (isGridView)?(<ProjectsGrid isVisible={ isGridView } />):(<ProjectsList/>)
