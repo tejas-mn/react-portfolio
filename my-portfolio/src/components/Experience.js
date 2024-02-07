03bad19095b0f00f92d3113b83e3365435c5ce5c
tejas-mn <68629215+tejas-mn@users.noreply.github.com>
2024-02-07 07:55:20 +0000

    refactor

--- my-portfolio/src/components/Experience.js
+++ my-portfolio/src/components/Experience.js
@@ -1,5 +1,22 @@
 import { exp } from "./DataProvider"
 
+function ExperienceDetail({e})
+{
+  return (
+    <>
+      <p key={e.id}>
+          <div style={{borderLeft:'3px solid lightblue', paddingLeft:'5px'}}>
+              <b>{e.company} </b>  <i style={{float:'right'}}> ({e.timeline})</i>  <br/>
+              <i>{e.title}</i> 
+          </div>
+          <ul>
+            { e.desc.map((d)=><li>{d}</li>) }
+          </ul>
+      </p>
+    </>
+  )
+}
+
 export function Experience(){
    
     return  (
@@ -8,20 +25,8 @@ export function Experience(){
         <p>
             Aspiring Software Engineer from computer science background, graduated in 2023 currently working as SDET in US based product company. Proficient in web development and backend programming with expertise in HTML, CSS, JavaScript, Python (Flask), SQL, C#, and React. Skilled in building robust and scalable applications, with a demonstrated ability to develop and deploy projects independently. Eager to leverage experience in development to contribute effectively to development teams in a challenging environment. Strong problem-solving skills combined with a passion for learning and adapting to new technologies.          
         </p>
-
-        {
-            exp.map((e)=> <>
-              <p key={e.id}>
-                <div style={{borderLeft:'3px solid lightblue', paddingLeft:'5px'}}>
-                    <b>{e.company} </b>  <i style={{float:'right'}}> ({e.timeline})</i>  <br/>
-                    <i>{e.title}</i> 
-                </div>
-                <ul>
-                  { e.desc.map((d)=><li>{d}</li>) }
-                </ul>
-              </p>
-            </>)
-        }
+        
+        { exp.map((ex) => <ExperienceDetail e={ex}/>) }
        
     </section>
     )
