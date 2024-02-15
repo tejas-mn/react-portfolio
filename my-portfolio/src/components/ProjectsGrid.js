1fa63ff54fb1bc0f1288620b2d2d77f8edfe70ad
tejas-mn-alt <159873533+tejas-mn-alt@users.noreply.github.com>
2024-02-15 13:37:45 +0000

    added modal

--- my-portfolio/src/components/ProjectsGrid.js
+++ my-portfolio/src/components/ProjectsGrid.js
@@ -2,13 +2,31 @@ import React from "react";
 import "./projects.css";
 import { ProfileLinks } from "./ProfileLinks";
 import { projects } from "../Providers/DataProvider";
+import { Modal } from "./Modal";
+import { useState } from "react";
+
 
 function ProjectsGrid({ isVisible }) {
+  const [modalOpen, setModalOpen] = useState(false);
+  const [currentProject, setCurrentProject] = useState({});
+
+  const openModal = (p) => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
   return (
     <>
       <div className={`project-grid ${isVisible ? " fade-in" : ""}`}>
         {projects.map((project) => (
-          <div key={project.id} className="project-box">
+          <div key={project.id} className="project-box"
+            onClick={() => {
+              openModal();
+              setCurrentProject(project);
+            }}>
             <div
               style={{
                 overflow: "hidden",
@@ -23,6 +41,27 @@ function ProjectsGrid({ isVisible }) {
             <p>{project.description}</p> */}
           </div>
         ))}
+
+        <Modal isOpen={modalOpen} onClose={closeModal}>
+          <h3 style={{ marginTop: '0' }}>{currentProject.title}</h3>
+          {/* <div>
+            <div className="_left"></div>
+            <div className="_right"></div>
+          </div> */}
+          <p className="project-list">
+
+            <i style={{ float: "left" }}>{currentProject.techStack}</i>
+            <i style={{ float: "right" }}> ({currentProject.timeline})</i>
+            <quote >
+              <a className="proj-link-list-btn"
+                href="https://github.com">Github<sup>↗</sup></a> | {" "}
+              <a className="proj-link-list-btn"
+                href="https://github.com">Live Link<sup>↗</sup></a></quote>
+            <ul>
+              <p className="auto-format">{currentProject.description}</p>
+            </ul>
+          </p>
+        </Modal>
       </div>
     </>
   );
