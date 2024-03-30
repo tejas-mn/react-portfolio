bc6b1612076ff443e3930924f708c97647ae84ec
tejas-mn <68629215+tejas-mn@users.noreply.github.com>
2024-03-30 09:59:35 +0000

    styled edu

--- my-portfolio/src/components/Right/Education.js
+++ my-portfolio/src/components/Right/Education.js
@@ -2,7 +2,8 @@ import { edu } from "../../Providers/DataProvider";
 
 function EducationDetail({ e }) {
   return (
-    <p id={e.id}>
+    <p id={e.id} style={styles.eduPara} className={"edu-p"+e.id}>
+      <span style={styles.dot}></span>
       <b>{e.title}</b>
       <i className="edu-timeline" style={styles.edu_timeline}>
         {" "}
@@ -15,7 +16,7 @@ function EducationDetail({ e }) {
 
 export default function Education() {
   return (
-    <section>
+    <section className="">
       <h2>🎓 Education</h2>
       {edu.map((e) => (
         <EducationDetail key={e.id} e={e} />
@@ -27,4 +28,16 @@ export default function Education() {
 const styles = {
   edu_timeline: { float: "right" },
   desc: { display: "block" },
+  eduPara: { position: "relative", borderLeft: "2px solid var(--text-color-light)", padding: "10px", margin: "0 20px 0px 20px" },
+  dot: {
+    background: 'var(--text-color-light)',
+    height: '15px',
+    position: 'absolute',
+    display: 'block',
+    top: '-8px',
+    left: '-8px',
+    width: '15px',
+    borderRadius: '100px',
+    zIndex: 2,
+  }
 };
