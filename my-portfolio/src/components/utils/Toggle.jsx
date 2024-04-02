d869fe13cac7cfe4e05d1f7130b251336c7d8733
tejas-mn <68629215+tejas-mn@users.noreply.github.com>
2024-04-02 10:12:47 +0000

    Added Toggle Switch

---
+++ my-portfolio/src/components/utils/Toggle.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import "./toggle.css"
+
+export const Toggle = ({ label, toggled, onClick }) => {
+    const [isToggled, toggle] = useState(toggled)
+
+    const callback = () => {
+        toggle(!isToggled)
+        onClick()
+    }
+
+    return (
+        <label className='toggle-label'>
+            <input className="toggle-input" type="checkbox" defaultChecked={isToggled} onClick={callback} />
+            <span className='toggle-text'>{label}</span>
+            {/* <strong className='toggle-strong'>{label}</strong> */}
+        </label>
+    )
+}
