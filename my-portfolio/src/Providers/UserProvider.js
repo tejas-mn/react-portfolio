cb03b19f181b4eb50e5bbe40d3474fe972ded83f
tejas-mn <68629215+tejas-mn@users.noreply.github.com>
2024-04-17 09:17:58 +0000

    Test preload banner

--- my-portfolio/src/Providers/UserProvider.js
+++ my-portfolio/src/Providers/UserProvider.js
@@ -15,7 +15,8 @@ const preloadImage = (url) => {
 
 const preloadProjectImages = async (projects) => {
     const preloadPromises = projects.map((project) => preloadImage(project.imageUrl));
-    await Promise.all(preloadPromises);
+    await Promise.all([...preloadPromises, preloadImage("https://i.pinimg.com/originals/cc/6a/fc/cc6afc451a6d3857d556331e16e10bf7.gif"), 
+    preloadImage("https://chosenoneappearal.com/cdn/shop/files/ezgif.com-gif-maker_-_2023-09-18T121549.841_1000x.gif?v=1695053769")]);
 };
 
 const userContext = createContext();
@@ -29,7 +30,7 @@ const UserProvider = ({ children }) => {
     }
     else if (loading) {
         console.log("Loading Data..")
-        return <FallbackLoader loader={CirclesLoader} />
+        // return <FallbackLoader loader={CirclesLoader} />
     } else {
 
         preloadProjectImages(data.projects)
