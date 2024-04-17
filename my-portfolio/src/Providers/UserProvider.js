d8dd656e23087c2773cbca7cd442a7c03a34d03e
tejas-mn <68629215+tejas-mn@users.noreply.github.com>
2024-04-17 08:15:36 +0000

    Preload images async

--- my-portfolio/src/Providers/UserProvider.js
+++ my-portfolio/src/Providers/UserProvider.js
@@ -4,6 +4,20 @@ import { createContext } from 'react';
 import FallbackLoader from '../components/utils/components/FallbackLoader';
 import { CirclesLoader } from '../components/utils/components/Loaders';
 
+const preloadImage = (url) => {
+    return new Promise((resolve, reject) => {
+        const img = new Image();
+        img.onload = () => resolve(img);
+        img.onerror = (err) => reject(err);
+        img.src = url;
+    });
+};
+
+const preloadProjectImages = async (projects) => {
+    const preloadPromises = projects.map((project) => preloadImage(project.imageUrl));
+    await Promise.all(preloadPromises);
+};
+
 const userContext = createContext();
 
 const UserProvider = ({ children }) => {
@@ -18,12 +32,14 @@ const UserProvider = ({ children }) => {
         return <FallbackLoader loader={CirclesLoader} />
     } else {
 
-        //preload project images 
-        data.projects.forEach(project => {
-            const img = new Image();
-            img.src = project.imageUrl;
-            data.projects.imageUrl = img.src;
-        });
+        preloadProjectImages(data.projects)
+            .then(() => {
+                console.log('All project images preloaded successfully');
+            })
+            .catch((error) => {
+                console.error('Failed to preload project images:', error);
+            });
+
 
         return (
             <userContext.Provider value={{
