b93429b0fb04fb682c3a17366b20bdd6aa55e1c9
tejas-mn <68629215+tejas-mn@users.noreply.github.com>
2024-08-24 11:57:16 +0000

    Uncomment ParticlesBackground

--- my-portfolio/src/components/utils/components/ParticlesBackground.jsx
+++ my-portfolio/src/components/utils/components/ParticlesBackground.jsx
@@ -1,106 +1,106 @@
 import React, { useMemo, useState, useEffect } from "react";
-import Particles, { initParticlesEngine } from "@tsparticles/react";
-import { loadFull } from "tsparticles";
+// import Particles, { initParticlesEngine } from "@tsparticles/react";
+// import { loadFull } from "tsparticles";
 import { useTheme } from "../../../Providers/ThemeProvider";
 
 function ParticlesBackground() {
-  const [isInitialized, setIsInitialized] = useState(false);
+  // const [isInitialized, setIsInitialized] = useState(false);
   const { theme } = useTheme();
 
-  useEffect(() => {
-    initParticles();
-  }, []);
+  // useEffect(() => {
+  //   initParticles();
+  // }, []);
 
-  const initParticles = async () => {
-    await initParticlesEngine(async (engine) => {
-      await loadFull(engine);
-    });
-    setIsInitialized(true);
-  };
+  // const initParticles = async () => {
+  //   await initParticlesEngine(async (engine) => {
+  //     await loadFull(engine);
+  //   });
+  //   setIsInitialized(true);
+  // };
 
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
+  // const particlesLoaded = (container) => {
+  //   console.log(container);
+  // };
 
   const bg = {
     "light-theme": "var(--index-bg-light)",
     "dark-theme": "var(--index-bg-dark)",
   };
 
-  const particleColor = {
-    "dark-theme": "#ffffff",
-    "light-theme": "#000000",
-  };
+  // const particleColor = {
+  //   "dark-theme": "#ffffff",
+  //   "light-theme": "#000000",
+  // };
 
-  const options = useMemo(() => {
-    return {
-      background: {
-        color: {
-          value: bg[theme],
-        },
-      },
-      fpsLimit: 200,
-      interactivity: {
-        events: {
-          onClick: {
-            enable: false,
-            mode: "push",
-          },
-          onHover: {
-            enable: false,
-            mode: "repulse",
-          },
-        },
-        modes: {
-          push: {
-            quantity: 4,
-          },
-          repulse: {
-            distance: 200,
-            duration: 0.4,
-          },
-        },
-      },
-      particles: {
-        color: {
-          value: particleColor[theme],
-        },
-        links: {
-          color: particleColor[theme],
-          distance: 150,
-          enable: true,
-          opacity: 0.5,
-          width: 1,
-        },
-        move: {
-          direction: "none",
-          enable: true,
-          outModes: {
-            default: "bounce",
-          },
-          random: false,
-          speed: 6,
-          straight: false,
-        },
-        number: {
-          density: {
-            enable: true,
-          },
-          value: 50,
-        },
-        opacity: {
-          value: 0.5,
-        },
-        shape: {
-          type: "circle",
-        },
-        size: {
-          value: { min: 1, max: 5 },
-        },
-      },
-      detectRetina: true,
-    };
-  }, [theme]);
+  // const options = useMemo(() => {
+  //   return {
+  //     background: {
+  //       color: {
+  //         value: bg[theme],
+  //       },
+  //     },
+  //     fpsLimit: 120,
+  //     interactivity: {
+  //       events: {
+  //         onClick: {
+  //           enable: true,
+  //           mode: "push",
+  //         },
+  //         onHover: {
+  //           enable: true,
+  //           mode: "repulse",
+  //         },
+  //       },
+  //       modes: {
+  //         push: {
+  //           quantity: 4,
+  //         },
+  //         repulse: {
+  //           distance: 200,
+  //           duration: 0.4,
+  //         },
+  //       },
+  //     },
+  //     particles: {
+  //       color: {
+  //         value: particleColor[theme],
+  //       },
+  //       links: {
+  //         color: particleColor[theme],
+  //         distance: 150,
+  //         enable: true,
+  //         opacity: 0.5,
+  //         width: 1,
+  //       },
+  //       move: {
+  //         direction: "none",
+  //         enable: true,
+  //         outModes: {
+  //           default: "bounce",
+  //         },
+  //         random: false,
+  //         speed: 6,
+  //         straight: false,
+  //       },
+  //       number: {
+  //         density: {
+  //           enable: true,
+  //         },
+  //         value: 80,
+  //       },
+  //       opacity: {
+  //         value: 0.5,
+  //       },
+  //       shape: {
+  //         type: "circle",
+  //       },
+  //       size: {
+  //         value: { min: 1, max: 5 },
+  //       },
+  //     },
+  //     detectRetina: true,
+  //   };
+  // }, [theme]);
 
   useEffect(() => {
     document.querySelector("body").style.background = bg[theme];
@@ -108,15 +108,15 @@ function ParticlesBackground() {
     // options.particles.links.color = particleColor[theme];
   }, [theme]);
 
-  return (
-    isInitialized && (
-      <Particles
-        id="tsparticles"
-        particlesLoaded={particlesLoaded}
-        options={options}
-      />
-    )
-  );
+  // return (
+  //   isInitialized && (
+  //     <Particles
+  //       id="tsparticles"
+  //       particlesLoaded={particlesLoaded}
+  //       options={options}
+  //     />
+  //   )
+  // );
 }
 
 export default ParticlesBackground;
